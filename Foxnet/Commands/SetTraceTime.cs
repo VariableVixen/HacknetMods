fb1ccb7a7987dfff84e94e120811cc0c41da6c10
Aryanna Morgan <amorgan@variablevixen.dev>
2025-07-04 00:12:25 -0400

    implement SetTraceTime command

--- Foxnet/Commands/SetTraceTime.cs
+++ Foxnet/Commands/SetTraceTime.cs
@@ -1,11 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Hacknet;
+
+using VariableVixen.Hacknet.Lib.Extensions;
 
 namespace VariableVixen.Hacknet.Foxnet.Commands;
 
-internal class SetTraceTime {
-	// TODO change trace timer
+internal class SetTraceTime: CommandBase {
+
+	public override string Description { get; } = "Set the passive trace time for the connected computer.";
+	public override string[] Arguments { get; } = ["seconds"];
+
+	public override void Execute(OS os, string cmd, string[] args) {
+		if (os.connectedComp is null) {
+			os.Print(Foxnet.MESSAGE_PREFIX, "You are not connected to a computer");
+			return;
+		}
+		if (!float.TryParse(args[0], out float time)) {
+			os.Print(Foxnet.MESSAGE_PREFIX, "Invalid argument: must pass trace time in seconds as a float");
+			return;
+		}
+		if (time < 0) {
+			os.Print(Foxnet.MESSAGE_PREFIX, "Invalid argument: trace time cannot be negative");
+			return;
+		}
+
+		os.connectedComp.traceTime = time;
+		if (time > 0)
+			os.Print(Foxnet.MESSAGE_PREFIX, $"Set trace time of {os.connectedComp.ip} to {time} seconds");
+		else
+			os.Print(Foxnet.MESSAGE_PREFIX, $"Cleared trace on {os.connectedComp.ip}");
+	}
 }
